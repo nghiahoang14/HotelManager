fc78a30a2bb4f961cec2ec2ffbd74a7537184feb
hoangganh1228 <hoanganh52521352@gmail.com>
2025-04-10 11:12:25 +0700

    branch hoangganh

--- index.php
+++ index.php
@@ -52,7 +52,7 @@
                             <input type="date" class="form-control shadow-none">
                         </div>
                         <div class="col-lg-3 mb-3">
-                            <label class="form-label" style="font-weight:500">Check-out </label>
+                            <label class="form-label" style="font-weight:500">Check-out - 1 </label>
                             <input type="date" class="form-control shadow-none">
                         </div>
                         <div class="col-lg-2 mb-3">
