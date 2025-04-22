51d8ca191a60b49f866a5b39842e170877788097
nghiahoang14 <nghiahoang814@gmail.com>
2025-04-22 20:43:13 +0700

    h1

--- index.php
+++ index.php
@@ -43,7 +43,7 @@
     <div class="container availability-form">
         <div class="row">
             <div class="col-lg-12 bg-white shadow p-4 rounded">
-                <h5 class="mb-4">Check Booking Availability - 11111</h5>
+                <h5 class="mb-4">Check Booking Availability</h5>
 
                 <form>
                     <div class="row align-items-end">
